8f401cf040cdada12d93ef393db6956dfa3f6891
Tharun-77 <152776677+Tharun-77@users.noreply.github.com>
2024-12-18 07:42:29 +0530

    Create boiler.java
    just a boiler plate code for easy to change

---
+++ boiler.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t=sc.nextInt();
+        
+        for(int q=0;q<t;q++){
+            
+        }
+    }
+}
