d6aeb6206621c8a45a447412559f7416622584d0
Tharun-77 <152776677+Tharun-77@users.noreply.github.com>
2025-07-10 21:21:07 +0530

    Create graphs.java
    added edges arry -> list

---
+++ graphs.java
@@ -0,0 +1,29 @@
+// This code is to convert edges array to ArrayList of graph pattern for a directed graph
+
+List<List<Integer>> list = new ArrayList<>();
+
+for(int i=0;i<V;i++){
+  list.add(new ArrayList<Integer>());
+}
+
+for(int i[] : edges){
+  int u = i[0];
+  int v = i[1];
+  list.get(u).add(v);
+}
+
+// This code is to convert edges array to ArrayList of graph pattern for a directed graph
+
+List<List<Integer>> list = new ArrayList<>();
+
+for(int i=0;i<V;i++){
+  list.add(new ArrayList<Integer>());
+}
+
+for(int i[] : edges){
+  int u = i[0];
+  int v = i[1];
+  list.get(u).add(v);
+  list.get(v).add(u);
+}
+
